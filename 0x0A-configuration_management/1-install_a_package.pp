ef2bb8d65fd8f5e3aa397c4184fbd7baebeada87
otiaudi <otiaudi9@gmail.com>
2023-11-25 10:02:47 +0300

    updated the file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
+# 1-install_a_package.pp
+
+# Ensure the package 'python3-pip' is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
 # Install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
+}
+
+# Notify a message when the installation is completed
+notify { 'Flask installed successfully':
+  require => Package['Flask'],
 }
