e839426eb1f5de0b37435c39fc99f1a832651b40
otiaudi <otiaudi9@gmail.com>
2023-11-25 09:58:17 +0300

    Install Flask version 2.1.0 using pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs puppet-lint, version 2.1.1
-
-package { 'puppet-lint':
-  ensure   => '2.1.10',
-  provider => 'gem',
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
